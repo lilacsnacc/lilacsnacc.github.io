79af40adae21ec1f209c8943809eb267442bfade
Lilac Snacc <lilacsnacc@gmail.com>
2023-07-30 22:22:58 -0400

    added "working on it" message to Projects section

--- src/sections/Projects/Projects.tsx
+++ src/sections/Projects/Projects.tsx
@@ -1,5 +1,13 @@
 import { Section } from '..'
 
+import ConstructionIcon from '@mui/icons-material/Construction'
+
 export function Projects() {
-  return <Section title='My Projects' />
+  return (
+    <Section title='My Projects'>
+      <ConstructionIcon />
+      working on it!
+      <ConstructionIcon />
+    </Section>
+  )
 }
